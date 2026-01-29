bc5e9c9718d370fbc7c98c82d60abac3a9ed75c2
ViniciusPeralta <vinicius-peralta@hotmail.com>
2026-01-29 18:27:13 -0300

    feat: create category entity

---
+++ src/main/java/com/cashflow/coredata/domain/entities/Category.java
@@ -0,0 +1,42 @@
+package com.cashflow.coredata.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "tb_category")
+public class Category implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String color;
+
+    @Column
+    private String icon;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @Column(nullable = false, name = "user_id")
+    private long userId;
+
+
+}
